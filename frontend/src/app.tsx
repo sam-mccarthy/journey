7a98f4f13be8a5f78798f01395ba360df22bdb7e
sam-mccarthy <samuelmccarthy03@gmail.com>
2025-01-25 20:09:24 -0500

    Added react-router-dom to route pages

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,9 +1,28 @@
 import './app.css'
+import {
+    StaticRouter,
+    Routes,
+    Route
+} from 'react-router-dom';
+import Post from "./components/post.tsx";
+import Landing from "./components/landing.tsx";
+import Login from "./components/login.tsx";
+import Signup from "./components/signup.tsx";
+import Profile from "./components/profile.tsx";
+import Journal from "./components/journal.tsx";
 
-function App() {
+function App(url: string) {
     return (
-        <>
-        </>
+        <StaticRouter location={ url }>
+            <Routes>
+                <Route path="/" Component={Landing} />
+                <Route path="/login" Component={Login} />
+                <Route path="/signup" Component={Signup} />
+                <Route path="/profile" Component={Profile} />
+                <Route path="/journal" Component={Journal} />
+                <Route path="/post" Component={Post} />
+            </Routes>
+        </StaticRouter>
     );
 }
 
--- frontend/src/entry-server.tsx
+++ frontend/src/entry-server.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react';
 import { renderToString } from 'react-dom/server';
 import App from './app';
 
-export function render(_url: string){
+export function render(url: string){
     const html = renderToString(
         <StrictMode>
-            <App />
+            <App url={ url } />
         </StrictMode>,
     );
 
